2e28c540b388a90b439876a8ac4a7a22d7687ed4
Remi <moremi.va@gmail.com>
2018-05-04 12:04:20 +0700

    Move to lts-8.13, add codegen too llvm

--- src/Parser.hs
+++ src/Parser.hs
@@ -2,6 +2,7 @@ module Parser where
 
 import Text.Parsec
 import Text.Parsec.String (Parser)
+import Control.Applicative ((<$>))
 
 import qualified Text.Parsec.Expr as Ex
 import qualified Text.Parsec.Token as Tok
@@ -9,38 +10,41 @@ import qualified Text.Parsec.Token as Tok
 import Lexer
 import Syntax
 
-binary s f assoc = Ex.Infix (reservedOp s >> return (BinOp f)) assoc
-
-table = [[binary "*" Times Ex.AssocLeft,
-          binary "/" Divide Ex.AssocLeft]
-        ,[binary "+" Plus Ex.AssocLeft,
-          binary "-" Minus Ex.AssocLeft]]
-
-floating :: Parser Expr
-floating = do
-  n <- float
-  return $ Float n
-
 int :: Parser Expr
 int = do
   n <- integer
   return $ Float (fromInteger n)
 
-extern :: Parser Expr
-extern = do
-  reserved "extern"
-  name <- identifier
-  args <- parens $ many variable
-  return $ Extern name args
+floating :: Parser Expr
+floating = Float <$> float
+
+binary s assoc = Ex.Infix (reservedOp s >> return (BinaryOp s)) assoc
+
+binops = [[binary "*" Ex.AssocLeft,
+          binary "/" Ex.AssocLeft]
+        ,[binary "+" Ex.AssocLeft,
+          binary "-" Ex.AssocLeft]]
+
+expr :: Parser Expr
+expr =  Ex.buildExpressionParser binops factor
+
+variable :: Parser Expr
+variable = Var <$> identifier
 
 function :: Parser Expr
 function = do
   reserved "def"
   name <- identifier
-  args <- parens $ many variable
+  args <- parens $ many identifier
   body <- expr
   return $ Function name args body
 
+extern :: Parser Expr
+extern = do
+  reserved "extern"
+  name <- identifier
+  args <- parens $ many identifier
+  return $ Extern name args
 
 call :: Parser Expr
 call = do
@@ -48,28 +52,17 @@ call = do
   args <- parens $ commaSep expr
   return $ Call name args
 
-variable :: Parser Expr
-variable = do
-  var <- identifier
-  return $ Var var
-
 factor :: Parser Expr
 factor = try floating
       <|> try int
-      <|> try extern
-      <|> try function
       <|> try call
-      <|> variable
-      <|> parens expr
+      <|> try variable
+      <|> (parens expr)
 
-expr :: Parser Expr
-expr = Ex.buildExpressionParser table factor
-
-toplevel :: Parser [Expr]
-toplevel = many $ do
-    def <- defn
-    reservedOp ";"
-    return def
+defn :: Parser Expr
+defn = try extern
+    <|> try function
+    <|> expr
 
 contents :: Parser a -> Parser a
 contents p = do
@@ -78,12 +71,11 @@ contents p = do
   eof
   return r
 
-defn :: Parser Expr
-defn = try extern
-    <|> try function
-    <|> expr
-
-
+toplevel :: Parser [Expr]
+toplevel = many $ do
+    def <- defn
+    reservedOp ";"
+    return def
 
 parseExpr :: String -> Either ParseError Expr
 parseExpr s = parse (contents expr) "<stdin>" s
--- src/Syntax.hs
+++ src/Syntax.hs
@@ -4,16 +4,10 @@ type Name = String
 
 data Expr
   = Float Double
-  | BinOp Op Expr Expr
   | Var String
   | Call Name [Expr]
-  | Function Name [Expr] Expr
-  | Extern Name [Expr]
-  deriving (Eq, Ord, Show)
-
-data Op
-  = Plus
-  | Minus
-  | Times
-  | Divide
+  | Function Name [Name] Expr
+  | Extern Name [Name]
+  | BinaryOp Name Expr Expr
+  | UnaryOp Name Expr
   deriving (Eq, Ord, Show)
